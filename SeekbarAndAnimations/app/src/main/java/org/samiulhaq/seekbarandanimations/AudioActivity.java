23c05c3c1a252b6954b2874406987f051660afc6
sami2905 <bsef18a028@pucit.edu.pk>
2021-03-31 00:04:30 +0500

    BSEF18A028_Sami-Ul-Haq

---
+++ SeekbarAndAnimations/app/src/main/java/org/samiulhaq/seekbarandanimations/AudioActivity.java
@@ -0,0 +1,24 @@
+package org.samiulhaq.seekbarandanimations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class AudioActivity extends AppCompatActivity {
+
+    MediaPlayer mp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio);
+        mp = MediaPlayer.create(this,R.raw.audio);
+    }
+    public void Play(View view) {
+        mp.start();
+    }
+    public void Pause(View view) {
+        mp.pause();
+    }
+}
